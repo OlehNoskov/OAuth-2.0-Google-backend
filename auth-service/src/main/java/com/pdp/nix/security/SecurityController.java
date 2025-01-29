498cd41805424a2765d66ad2ef6b77edf037b860
Noskov, Oleh [PS] <oleh.noskov@cengage.com>
2025-01-29 13:46:44 +0200

    Add banner and fix issue with Controller

--- auth-service/src/main/java/com/pdp/nix/security/SecurityController.java
+++ auth-service/src/main/java/com/pdp/nix/security/SecurityController.java
@@ -1,4 +1,4 @@
-package com.pdp.security;
+package com.pdp.nix.security;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
