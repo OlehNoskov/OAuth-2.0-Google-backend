cbed96b26b42b68778144c68bf775d9f2458b01b
Noskov, Oleh [PS] <oleh.noskov@cengage.com>
2025-01-29 21:04:02 +0200

    Add spring security (Google OAuth)

--- auth-service/src/main/java/com/pdp/nix/security/SecurityController.java
+++ auth-service/src/main/java/com/pdp/nix/security/SecurityController.java
@@ -1,15 +1,18 @@
 package com.pdp.nix.security;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
-@RequestMapping("/ping")
 public class SecurityController {
 
-    @GetMapping()
+    @GetMapping("/ping")
     public String ping() {
         return "pong";
+    }
+
+    @GetMapping("/secured")
+    public String secured() {
+        return "Hello! This is a secured service";
     }
 }
